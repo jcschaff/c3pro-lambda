36695140910a6424aacee2922bd0f70f2ad7d3a3
Jim Schaff <schaff@uchc.edu>
2017-12-28 14:46:19 -0500

    add /register endpoint for testing

--- src/main/java/com/aws/codestar/projecttemplates/handler/RegisterHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/RegisterHandler.java
@@ -11,11 +11,11 @@ import com.aws.codestar.projecttemplates.GatewayResponse;
 /**
  * Handler for requests to Lambda function.
  */
-public class HelloWorldHandler implements RequestHandler<Object, Object> {
+public class RegisterHandler implements RequestHandler<Object, Object> {
 
     public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        return new GatewayResponse("{ \"Output\": \"Hello World 3!\"}", headers, 200);
+        return new GatewayResponse("{ \"Output\": \"RegistrationResponse\"}", headers, 200);
     }
 }
